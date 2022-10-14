c3847eb544fa289e5fd0a320e86214faf8de6c94
WangChiech <wangchiech@yeah.net>
2022-10-14 12:26:56 +0800

    step2 render

--- src/react/render.js
+++ src/react/render.js
@@ -1,5 +1,18 @@
 function render (vDOM, container) {
   console.log('---render---', vDOM, container)
+  const dom = vDOM.type === 'TEXT ELEMENT'
+    ? document.createTextNode(vDOM.props.nodeValue)
+    : document.createElement(vDOM.type)
+  vDOM.props.children.forEach(child => {
+    render(child, dom)
+  })
+  const domKeys = Object.keys(vDOM.props)
+  domKeys.forEach(propName => {
+    if (propName !== 'children') {
+      dom[propName] = vDOM.props[propName]
+    }
+  })
+  container.appendChild(dom)
 }
 
 export default render
